8047135bf24251e1f81d2c6ba6d4e71e160f3202
Roby <476350447@qq.com>
2018-07-20 12:23:43 +0800

    Initial commit

---
+++ src/ArrayTest.java
@@ -0,0 +1,85 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayTest {
+
+//	Boolean[]a=new Boolean[10]; 
+//	public void myMethod(Object o)
+//	{ 
+//		System.out.println("MyObject");
+//	} 
+	
+//	public void myMethod(String s)
+//	{ 
+//		System.out.println("MyString");
+//	} 
+	
+	public static void main(String[] args)
+	{
+//		// TODO Auto-generated method stub
+//		int a1[] = {1,2,3,4,5};
+//		int a2[];
+//		a2=a1;
+//		for (int i = 0; i < a2.length; i++) 
+//		{
+//			a2[i]++;
+//		}
+//		for (int j = 0; j < a1.length; j++)
+//		{
+//			System.out.println(a1[j]);
+//		}
+//		
+//		int a=0XFFFFFF1;
+//		System.out.println(Integer.MAX_VALUE);
+//		System.out.println("a 非的结果是："+(~a));
+		
+//		int i=9;
+//		switch (i){ 
+//		case 1: System.out.print("1");
+//		default: System.out.print("Error");
+//		case 2: System.out.print("Good");
+//		case 3: System.out.print("Best");
+//		}
+		
+//		boolean a[] = new boolean[4];
+//		System.out.println(a[0]);
+		
+//				ArrayTest demo = new ArrayTest();
+//				demo.display();
+//				Boolean b[] = new Boolean[5];
+//				System.out.println(b[0]);
+//			
+//	}
+//	void display()
+//	{ 
+//		System.out.println(a[9]);
+//	}
+		
+		
+//			ArrayTest t = new ArrayTest();
+//			t.myMethod(null);
+		
+//		byte a = 2;
+//		byte b = 3;
+//		byte c = (byte) (a+b);
+//		String[] ab = "|DF|A3".split("\\|");
+//		for (int i = 0; i < ab.length; i++) {
+//			
+//			System.out.println(ab[i]);
+//		}
+//		
+//		String str = "abcdefg";
+//		System.out.println(str.substring(2,5));
+
+	String a = "hello2";
+	final String b = "hello";
+	String d = "hello";
+	String c = b + 2;
+	String e = d + 2;
+	System.out.println(c);
+	System.out.println(e);
+	System.out.println((a == c));
+	System.out.println((a == e));
+    }
+}
+
